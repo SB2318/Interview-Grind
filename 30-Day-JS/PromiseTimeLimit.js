4b828faaafb967528f19a0901f3d4d57f24aabd3
S. B. | Software Developer <87614560+SB2318@users.noreply.github.com>
2026-04-24 21:04:02 +0530

    Create PromiseTimeLimit.js

---
+++ 30-Day-JS/PromiseTimeLimit.js
@@ -0,0 +1,30 @@
+// Problem Link: https://leetcode.com/problems/promise-time-limit/
+
+// Promise.race :
+// Promise.race() is a static method in JavaScript that takes an iterable of promises and 
+//returns a single Promise that settles as soon as the first promise in the iterable settles (either fulfills or rejects).
+
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function(fn, t) {
+    
+    return async function(...args) {
+         const originalFnPromise = fn(...args);
+
+        const timeoutPromise = new Promise((_, reject) => {
+            setTimeout(() => {
+                reject('Time Limit Exceeded')
+            }, t);
+        })
+
+        return Promise.race([originalFnPromise, timeoutPromise]);
+    }
+};
+
+/**
+ * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+ * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
+ */
