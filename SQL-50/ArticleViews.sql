0d35184ebf8a60ef43f0c93338f2be1241703bc9
S. B. | Software Developer <87614560+SB2318@users.noreply.github.com>
2026-05-08 23:21:53 +0530

    Enhance ArticleViews.sql with ORDER BY explanation
    Added comments explaining the ORDER BY clause usage.

--- SQL-50/ArticleViews.sql
+++ SQL-50/ArticleViews.sql
@@ -1,4 +1,6 @@
 # Link: https://leetcode.com/problems/article-views-i/
 
-# Write your MySQL query statement below
+# OrderBy:
+# ORDER BY clause is used to sort the results of a query in either ascending (asc) or descending order (desc). By default, it sorts data in ascending order (ASC)
+  
 SELECT Distinct author_id as id from views where author_id = viewer_id order by author_id asc;
