8b2344156c2289017fd9ab5c275ebac8c3f1f414
S. B. | Software Developer <87614560+SB2318@users.noreply.github.com>
2026-05-08 23:19:52 +0530

    Update InvalidTweet.sql

--- SQL-50/InvalidTweet.sql
+++ SQL-50/InvalidTweet.sql
@@ -1,4 +1,9 @@
 // Link: https://leetcode.com/problems/invalid-tweets/
+  
+# In SQL, the LENGTH function is used to determine the size of a string
+# In MySQL, LENGTH() returns the number of bytes
+# In PostgreSQL and Oracle, LENGTH() returns the number of Characters
+# In SQL Server, LEN() returns Characters (excludes trailing spaces)
+# In SQLite, LENGTH() returns Characters
 
-# Write your MySQL query statement below
 SELECT tweet_id from Tweets where LENGTH(content) > 15;
