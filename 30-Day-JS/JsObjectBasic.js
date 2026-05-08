86fb35a44ed261b07b6b02c39ccf91d5dd7cf1d1
S. B. | Software Developer <87614560+SB2318@users.noreply.github.com>
2026-05-08 21:09:03 +0530

    Create JsObjectBasic.js

---
+++ 30-Day-JS/JsObjectBasic.js
@@ -0,0 +1,10 @@
+// Link: https://leetcode.com/problems/is-object-empty/
+
+/**
+ * @param {Object|Array} obj
+ * @return {boolean}
+ */
+var isEmpty = function(obj) {
+    
+    return Object.keys(obj).length == 0;
+};
