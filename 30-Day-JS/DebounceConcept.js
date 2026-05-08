b77924030ba662ecdedef970192f2385847e70e9
S. B. | Software Developer <87614560+SB2318@users.noreply.github.com>
2026-05-08 20:26:25 +0530

    Add debounce concept explanation in comments
    Added a comment explaining the debounce concept.

--- 30-Day-JS/DebounceConcept.js
+++ 30-Day-JS/DebounceConcept.js
@@ -2,7 +2,6 @@
 
 /**
 
-Debounce concept : cancel old, keep latest.
 A debounced function is a function whose execution is delayed by t milliseconds and whose execution is cancelled if it is called again within that window of time. 
 The debounced function should also receive the passed parameters.
 
