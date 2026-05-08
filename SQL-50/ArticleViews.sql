ad19a41286ef910e03eacdaf9639465623bf686d
S. B. | Software Developer <87614560+SB2318@users.noreply.github.com>
2026-05-08 22:34:57 +0530

    Create ArticleViews.sql

---
+++ SQL-50/ArticleViews.sql
@@ -0,0 +1,4 @@
+# Link: https://leetcode.com/problems/article-views-i/
+
+# Write your MySQL query statement below
+SELECT Distinct author_id as id from views where author_id = viewer_id order by author_id asc;
