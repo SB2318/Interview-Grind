53b8c7a4aa0a154f1a944f96f711f1d2a9b2ae2c
S. B. | Software Developer <87614560+SB2318@users.noreply.github.com>
2026-05-08 20:22:39 +0530

    Create DebounceConcept.js

---
+++ 30-Day-JS/DebounceConcept.js
@@ -0,0 +1,33 @@
+// Link: https://leetcode.com/problems/debounce/?envType=study-plan-v2&envId=30-days-of-javascript
+
+/**
+
+Debounce concept : cancel old, keep latest.
+A debounced function is a function whose execution is delayed by t milliseconds and whose execution is cancelled if it is called again within that window of time. 
+The debounced function should also receive the passed parameters.
+
+30ms -> call A
+schedule A for 80ms
+
+60ms -> call B (inside A's 50ms waiting window)
+cancel A
+schedule B for 110ms
+
+100ms -> call C (inside B's waiting window)
+cancel B
+schedule C for 150ms
+*/
+
+/**
+ * @param {Function} fn
+ * @param {number} t milliseconds
+ * @return {Function}
+ */
+var debounce = function(fn, t) {
+    
+    let timer;
+    return function(...args) {
+       clearTimeout(timer);
+       timer = setTimeout(()=> fn(...args), t);
+    }
+};
